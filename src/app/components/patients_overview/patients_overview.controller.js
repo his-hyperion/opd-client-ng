3f2de354dec1a4a67dbd058998a16e8f682879a9
Shalitha Mihiranga <Shalitha Mihiranga>
2017-05-09 11:32:52 +0530

    patient overview get patients list

--- src/app/components/patients_overview/patients_overview.controller.js
+++ src/app/components/patients_overview/patients_overview.controller.js
@@ -1,68 +1,85 @@
 angular
     .module('patientOverview')
-    .controller('patientsOverviewController', ['$scope', function ($scope) {
-        $scope.name = "Mr.Mahesh";
-        $scope.HIN = "12542252";
-        $scope.gender = "Male";
-        $scope.civilStatus = "Married";
-        $scope.dob = "09-09-1990";
-        $scope.phone = "07221654789";
-        $scope.address = "Colombo";
-        $scope.bloodGroup = "A+";
-        $scope.BMI = "Not Calculated";
+    .controller('patientsOverviewController', ['$scope', '$http', function ($scope, $http) {
+
+        $scope.patients = []
+
+        $scope.getPatients = function () {
+            $http.get('http://localhost:8080/api/patients')
+                .then(function (response) {
+                    $scope.patients = response.data;
+                    // console.log($scope.patients[0].firstName);
+                });
+        };
+        $scope.selectPatient = function (name) {
+            for (var i = 0; i < $scope.patients.length; i++) {
+                if ($scope.patients[i].firstName == name) {
+                    $scope.name = $scope.patients[i].firstName + " " + $scope.patients[i].lastName;
+                    $scope.HIN = "12542252";
+                    $scope.gender = $scope.patients[i].gender;
+                    $scope.civilStatus = $scope.patients[i].civilStatus;
+                    $scope.dob = $scope.patients[i].dob;                   
+                    $scope.address = $scope.patients[i].address;
+                    $scope.phone = $scope.patients[i].phone;
+                    $scope.mobile = $scope.patients[i].mobile;
+                    $scope.bloodGroup = $scope.patients[i].bloodGroup;
+                    $scope.BMI = "Not Calculated";
+                }
+            }
+        };
 
         $scope.getVisits = function () {
-            $scope.visits= [{
-                visitType:"OPD",
-                visitNo : "001",
-                dateTime : "2017-05-08",
-                complaint : "aa",
-                remark : ""
+            $scope.visits = [{
+                visitType: "OPD",
+                visitNo: "001",
+                dateTime: "2017-05-08",
+                complaint: "aa",
+                remark: ""
             },
             {
-                visitType:"OPD",
-                visitNo : "002",
-                dateTime : "2017-05-07",
-                complaint : "sdff",
-                remark : "ds"
+                visitType: "OPD",
+                visitNo: "002",
+                dateTime: "2017-05-07",
+                complaint: "sdff",
+                remark: "ds"
             }];
         };
 
-        $scope.getExaminations = function(){
+        $scope.getExaminations = function () {
             $scope.examinations = [{
-                date:"2017-05-08",
-                weight:"78",
-                height:"168",
-                SBP:"33",
-                DBP:"42",
-                temperature:"33"
+                date: "2017-05-08",
+                weight: "78",
+                height: "168",
+                SBP: "33",
+                DBP: "42",
+                temperature: "33"
             },
             {
-                date:"2017-04-23",
-                weight:"78",
-                height:"168",
-                SBP:"35",
-                DBP:"42",
-                temperature:"28"
+                date: "2017-04-23",
+                weight: "78",
+                height: "168",
+                SBP: "35",
+                DBP: "42",
+                temperature: "28"
             }]
         };
-        $scope.getLabs = function(){
+        $scope.getLabs = function () {
             $scope.labs = [{
-                name:"AAA",
-                date:"2017-05-08",
-                status:"Report issued"
+                name: "AAA",
+                date: "2017-05-08",
+                status: "Report issued"
             },
             {
-                name:"BBB",
-                date:"2017-04-23",             
-                status:"Testing",
+                name: "BBB",
+                date: "2017-04-23",
+                status: "Testing",
             }]
         };
-        $scope.getAllergies = function(){
+        $scope.getAllergies = function () {
             $scope.labs = [{
-                name:"",
-                date:"",
-                status:""
+                name: "",
+                date: "",
+                status: ""
             }]
         }
     }]);
