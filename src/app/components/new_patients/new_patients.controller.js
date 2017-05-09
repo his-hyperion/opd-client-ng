e2319ff6d37df6e368a2cd09fb96560d64c757ac
Shalitha Mihiranga <Shalitha Mihiranga>
2017-05-09 09:58:08 +0530

    Insert new patient

--- src/app/components/new_patients/new_patients.controller.js
+++ src/app/components/new_patients/new_patients.controller.js
@@ -1,6 +1,6 @@
 angular
     .module('newPatients')
-    .controller('newPatientsController', ['$scope', '$mdDialog', function ($scope, $mdDialog) {
+    .controller('newPatientsController', ['$scope', '$mdDialog', '$http', function ($scope, $mdDialog, $http) {
 
         //datepicker validation not to select future dates
         $scope.myDate = new Date();
@@ -10,8 +10,8 @@ angular
             $scope.myDate.getDate()
         );
         // add new patients
+        $scope.newPatient = {};
         $scope.AddNewPatients = function () {
-            $scope.newPatient = {};
 
             var formData = new FormData();
             angular.forEach($scope.files, function (obj) {
@@ -19,7 +19,7 @@ angular
                 formData.append('files[]', obj.lfFile);
 
             });
-            console.log(formData)
+            //console.log(formData)
 
 
             $scope.newPatient.title = $scope.title;
@@ -38,17 +38,21 @@ angular
             $scope.newPatient.mobile = $scope.mobile;
             $scope.newPatient.remark = $scope.remark;
 
-            $mdDialog.show(
-                $mdDialog.alert()
-                    .parent(angular.element(document.querySelector('#popupContainer')))
-                    .clickOutsideToClose(true)
-                    .title('Success')
-                    .textContent('New patient details successfuly added to the database.')
-                    .ariaLabel('Alert Dialog Demo')
-                    .ok('Got it!')
-            );
-            console.log($scope.newPatient);
-            $scope.Reset();
+            $http.post('http://localhost:8080/api/patients', $scope.newPatient)
+                .then(function (response) {
+                    // clear form
+                    $mdDialog.show(
+                        $mdDialog.alert()
+                            .parent(angular.element(document.querySelector('#popupContainer')))
+                            .clickOutsideToClose(true)
+                            .title('Success')
+                            .textContent('New patient details successfuly added to the database.')
+                            .ariaLabel('Alert Dialog Demo')
+                            .ok('Got it!')
+                    );
+                    $scope.Reset();
+                });
+
 
         };
 
