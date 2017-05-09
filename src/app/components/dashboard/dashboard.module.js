ae48792da91ab664c7fa2b7cf22811784470840d
Mahendra Tennakoon <mahendra.tennakoon@pearson.com>
2017-05-09 13:35:47 +0530

    dashboard patients over time

---
+++ src/app/components/dashboard/dashboard.module.js
@@ -0,0 +1,43 @@
+angular.module('dashboard', ['chart.js'])
+    .controller('dashboardController', function ($scope) {
+        $scope.labels = ["Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"];
+        $scope.series = ['New', 'Recurring'];
+        $scope.data = [
+            [6, 5, 12, 6, 5, 5, 4],
+            [2, 4, 4, 1, 1, 2, 9]
+        ];
+        $scope.onClick = function (points, evt) {
+            console.log(points, evt);
+        };
+        $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+        $scope.options = {
+            scales: {
+                yAxes: [
+                    {
+                        id: 'y-axis-1',
+                        type: 'linear',
+                        display: true,
+                        position: 'left',
+                        ticks: {
+                            // fontColor: "#F8F8DD", // this here
+                        }
+                    },
+                    {
+                        id: 'y-axis-2',
+                        type: 'linear',
+                        display: true,
+                        position: 'right',
+                        ticks: {
+                            // fontColor: "#F8F8DD", // this here
+                        }
+                    }
+                ]
+            },
+            legend: {
+                display: true,
+                labels: {
+                    // fontColor: '#FEC44F'
+                }
+            }
+        };
+    });
