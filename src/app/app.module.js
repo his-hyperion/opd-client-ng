4d003b1d4b11950d3f8576203fdd3a6cb7da6dd5
Shalitha Mihiranga <shalitha.mihi99@gmail.com>
2017-06-28 00:56:40 +0530

    resolve mearge conflict

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,6 +1,6 @@
 angular.module('opdClient', ['ngMaterial', 'ngMessages','md.data.table','lfNgMdFileInput', 'sidebar', 
 'ui.router', 'myPatients', 'newPatients','patientOverview', 'questionnaire', 'dashboard', 'prescribeDrugs', 
-'newExaminations'])
+'newExaminations', 'updatePatients'])
     .run(function () {
         
     });
