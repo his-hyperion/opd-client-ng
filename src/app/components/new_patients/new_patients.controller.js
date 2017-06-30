ae4bd8da520a8fc5323301843df03c7efd98f996
Shalitha Mihiranga <shalitha.mihi99@gmail.com>
2017-07-01 00:45:47 +0530

    create patient card

--- src/app/components/new_patients/new_patients.controller.js
+++ src/app/components/new_patients/new_patients.controller.js
@@ -25,16 +25,46 @@ angular
 
             patientsService.addNewPatient($scope.newPatient)
                 .then(function (response) {
-                    // clear form
-                    $mdDialog.show(
-                        $mdDialog.alert()
-                            .parent(angular.element(document.querySelector('#popupContainer')))
-                            .clickOutsideToClose(true)
-                            .title('Success')
-                            .textContent('New patient details successfuly added to the database.')
-                            .ariaLabel('Alert Dialog Demo')
-                            .ok('Got it!')
-                    );
+                    $mdDialog.show({
+                        clickOutsideToClose: true,
+                        scope: $scope,
+                        preserveScope: true,
+                        template: '<md-dialog><md-toolbar>' +
+                        '<div class="md-toolbar-tools">' +
+                        '<h2>Patient Card</h2>' +
+                        '<span flex></span></div>' +
+                        '</md-toolbar>' +
+                        '<md-dialog-content>' +
+                        '<md-input-container flex=25"></md-input-container>'+
+                        '<md-input-container flex="120">'+
+                        '<table><tbody>'+
+                        '<tr>'+
+                        '<td><h4> Patient Name :</h4></td>'+
+                        '<td><h4>'+ $scope.newPatient.title +$scope.newPatient.firstName +'</h4></td>'+
+                        '<td><h4>'+ $scope.newPatient.lastName +'</h4></td>'+
+                        '</tr>'+
+                        '<tr>'+
+                        '<td><h4> Patient Hin :</h4></td>'+
+                        '<td><h4>'+ $scope.newPatient.hin +'</h4></td>'+
+                        '</tr>'+
+                        '<tr>'+
+                        '<td><h4></h4></td>'+
+                        '<td><img src="./src/assets/img/barcode.jpg" height="52" width="100" class="md-card-image"></td>'+
+                        '</tr>'+
+                        '</tbody></table>'+
+                        '</md-input-container><br/>'+
+                        '<md-input-container flex=25"></md-input-container>'+
+                        '<md-input-container flex="120">'+
+                        '<h4>Bring this card with you on every visit to the hospital.</h4>'+
+                        '</md-input-container><br/>'+
+                        '</md-dialog-content>' +
+                        '</md-dialog>',
+                        controller: function DialogController($scope, $mdDialog) {
+                            $scope.closeDialog = function () {
+                                $mdDialog.hide();
+                            }
+                        }
+                    });
                     $scope.Reset();
                 });
 
