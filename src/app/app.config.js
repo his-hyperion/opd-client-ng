129675738aa73645e7b6769344ace2d00b7cd971
Mahendra Tennakoon <mahendra.tennakoon@pearson.com>
2017-06-30 11:55:58 +0530

    set default state

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -1,39 +1,44 @@
 angular.module('opdClient')
-    .config(['$stateProvider', '$mdThemingProvider',
-        function ($stateProvider, $mdThemingProvider) {
+    .config(['$stateProvider', '$urlRouterProvider', '$mdThemingProvider',
+        function ($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+            var loginState = {
+                name: 'login',
+                url: '/login',
+                templateUrl: 'components/login/login.html'
+            };
             var dashboardState = {
                 name: 'dashboard',
-                url: 'dashboard',
+                url: '/dashboard',
                 templateUrl: 'components/dashboard/dashboard.html'
             };
             var patientState = {
                 name: 'patients',
-                url: 'patients',
+                url: '/patients',
                 templateUrl: 'components/my_patients/my_patients.html'
             };
             var newPatientState = {
                 name: 'newPatients',
-                url: 'newPatients',
+                url: '/newPatients',
                 templateUrl: 'components/new_patients/new_patients.html'
             };
             var drugPrescriptionState = {
                 name: 'prescribeDrugs',
-                url: 'prescribeDrugs',
+                url: '/prescribeDrugs',
                 templateUrl: 'components/prescribe_drugs/prescribe_drugs.html'
             };
             var patientOverviewState = {
                 name: 'patientOverview',
-                url: 'patientOverview',
+                url: '/patientOverview',
                 templateUrl: 'components/patients_overview/patients_overview.html'
             };
             var questionnaireState = {
                 name: 'questionnaire',
-                url: 'questionnaire',
+                url: '/questionnaire',
                 templateUrl: 'components/questionnaire/questionnaire.html'
             };
             var newExaminationsState = {
                 name: 'newExaminations',
-                url: 'newExaminations',
+                url: '/newExaminations',
                 templateUrl: 'components/new_examinations/new_examinations.html'
             };
 
@@ -44,6 +49,11 @@ angular.module('opdClient')
             $stateProvider.state(patientOverviewState);
             $stateProvider.state(questionnaireState);
             $stateProvider.state(newExaminationsState);
+            
+            $urlRouterProvider.otherwise(function ($injector, $location) { 
+                var $state = $injector.get('$state'); 
+                $state.go(dashboardState);
+            });
 
             $mdThemingProvider.theme('dark-grey')
                 .backgroundPalette('grey')
@@ -51,4 +61,4 @@ angular.module('opdClient')
             $mdThemingProvider.theme('docs-dark', 'default')
                 .primaryPalette('yellow')
                 .dark();
-        }])
+        }]);
